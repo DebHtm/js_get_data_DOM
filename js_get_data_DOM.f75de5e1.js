f3832c2e5ce49f8e3e968c348789f899b854d940
DebHtm <yolm9yolm@gmail.com>
2025-08-21 19:06:19 +0300

    make build

---
+++ js_get_data_DOM.f75de5e1.js
@@ -0,0 +1,15 @@
+'use strict';
+const arr = [
+    ...document.querySelectorAll('.population')
+];
+const result = arr.map((el)=>el.textContent?.trim().replace(/,/g, '')).filter((s)=>s !== '' && Number.isFinite(Number(s))).map((s)=>Number(s));
+let total = 0;
+let average = 0;
+if (result.length > 0) {
+    total = result.reduce((sum, num)=>sum + num, 0);
+    average = total / result.length;
+}
+document.querySelector('.total-population').textContent = total.toLocaleString('en-US');
+document.querySelector('.average-population').textContent = average.toLocaleString('en-US');
+
+//# sourceMappingURL=js_get_data_DOM.f75de5e1.js.map
