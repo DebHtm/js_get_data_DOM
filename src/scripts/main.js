bfeaf890e6a30652229a7d45570f14c2c7744caf
DebHtm <yolm9yolm@gmail.com>
2025-08-21 18:40:14 +0300

    DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const arr = [...document.querySelectorAll('.population')];
+const result = arr.map((text) => Number(text.textContent.replaceAll(',', '')));
+const total = result.reduce((sum, num) => sum + num, 0);
+const average = total / result.length;
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString('en-US');
